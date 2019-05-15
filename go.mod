d3b4208f96118787280e30e536f59f432920c51f
Michal Jemala <michal.jemala@gmail.com>
2019-05-15 23:18:01 +0200

    Add schema compare CLI tool

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.12
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/vektah/gqlparser v1.1.2
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- pkg/schema/compare/compare.go
+++ pkg/schema/compare/compare.go
@@ -6,6 +6,9 @@ import (
 	"io/ioutil"
 )
 
+// Compare compares two GraphQL schemas and returns a set of detected changes.
+// Each change has a severity and reason assigned to be able to further evaluate its impact.
+// Schemas must be encoded using SDL, no other form is accepted.
 func Schema(x, y io.Reader) (*Result, error) {
 	parseSchema := func(name string, r io.Reader) (*schema, error) {
 		b, err := ioutil.ReadAll(r)
@@ -33,36 +36,95 @@ func Schema(x, y io.Reader) (*Result, error) {
 	return r, nil
 }
 
+// Result stores the detected changes.
 type Result struct {
-	changes []Change
+	breaking    []Change
+	dangerous   []Change
+	nonBreaking []Change
 }
 
 func (r *Result) reportChange(c Change) {
-	r.changes = append(r.changes, c)
+	switch l := c.Severity.Level; l {
+	case Breaking:
+		r.breaking = append(r.breaking, c)
+	case Dangerous:
+		r.dangerous = append(r.dangerous, c)
+	case NonBreaking:
+		r.nonBreaking = append(r.nonBreaking, c)
+	default:
+		panic(fmt.Errorf("invalid severity level: %s", l))
+	}
 }
 
+// Breaking returns list of changes which are not backward compatible.
+func (r Result) Breaking() []Change {
+	return r.breaking
+}
+
+// Dangerous returns list of changes which in some cases may be considered breaking.
+func (r Result) Dangerous() []Change {
+	return r.dangerous
+}
+
+// NonBreaking returns list of changes which are backward compatible.
+func (r Result) NonBreaking() []Change {
+	return r.nonBreaking
+}
+
+// Changes returns list of all changes.
 func (r Result) Changes() []Change {
-	return r.changes
+	var changes []Change
+	for _, c := range r.breaking {
+		changes = append(changes, c)
+	}
+	for _, c := range r.dangerous {
+		changes = append(changes, c)
+	}
+	for _, c := range r.nonBreaking {
+		changes = append(changes, c)
+	}
+	return changes
 }
 
+// Change materialize a schema modification.
 type Change struct {
+
+	// Severity of the change
 	Severity ChangeSeverity
-	Type     ChangeType
-	Message  string
-	Path     string
+
+	// Type of the change
+	Type ChangeType
+
+	// Message provides human-readable explanation of the change
+	Message string
+
+	// Path to the changed item
+	Path string
 }
 
+// ChangeSeverity defined how serious a change is.
 type ChangeSeverity struct {
-	Level  ChangeSeverityLevel
+
+	// Level indicates backward compatibility
+	Level ChangeSeverityLevel
+
+	// Reason provides human-readable explanation of why the severity is chosen
 	Reason string
 }
 
+// Level indicates backward compatibility.
 type ChangeSeverityLevel string
 
 const (
-	Breaking    = ChangeSeverityLevel("BREAKING")
+	// Breaking causes existing clients to error
+	Breaking = ChangeSeverityLevel("BREAKING")
+
+	// Dangerous may in some cases cause existing clients to error
+	Dangerous = ChangeSeverityLevel("DANGEROUS")
+
+	// NonBreaking keeps schema backward compatible
 	NonBreaking = ChangeSeverityLevel("NON_BREAKING")
-	Dangerous   = ChangeSeverityLevel("DANGEROUS")
 )
 
+// ChangeType allows categorization of changes.
 type ChangeType string
